8875edd2ad5badbcfcd46358ba50268b7e7e4634
MinexAutomation <minex.automation@gmail.com>
2020-07-04 12:56:00 -0700

    Added R5T.D0027.Default library. Added MachineNameProvider service implementation class type.

---
+++ source/R5T.D0027.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0027.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="MachineNameProvider"/> implementation of <see cref="IMachineNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddMachineNameProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IMachineNameProvider, MachineNameProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="MachineNameProvider"/> implementation of <see cref="IMachineNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IMachineNameProvider> AddMachineNameProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<IMachineNameProvider>(() => services.AddMachineNameProvider());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0027.Default/Code/Services/Implementations/MachineNameProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.D0027.Default
+{
+    /// <summary>
+    /// Provides the <see cref="Environment.MachineName"/> value.
+    /// </summary>
+    public class MachineNameProvider : IMachineNameProvider
+    {
+        public string GetMachineName()
+        {
+            var machineName = Environment.MachineName;
+            return machineName;
+        }
+    }
+}
