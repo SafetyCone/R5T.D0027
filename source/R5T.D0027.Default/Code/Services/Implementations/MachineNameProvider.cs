1f10f477c9f34997d76eecc50e18144c7f3ad542
davidcoats <david@getsafetycone.com>
2022-12-19 21:21:52 -0800

    Interim changes.

--- source/R5T.D0027.Default/Code/Services/Implementations/MachineNameProvider.cs
+++ source/R5T.D0027.Default/Code/Services/Implementations/MachineNameProvider.cs
@@ -2,10 +2,11 @@ using System;using R5T.T0064;
 
 
 namespace R5T.D0027.Default
-{[ServiceImplementationMarker]
+{
     /// <summary>
     /// Provides the <see cref="Environment.MachineName"/> value.
     /// </summary>
+    [ServiceImplementationMarker]
     public class MachineNameProvider : IMachineNameProvider,IServiceImplementation
     {
         public string GetMachineName()
